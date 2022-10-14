a2d3d5c1a4621d39257a21af45ed587c63638b5c
Avighna <avighnakc@gmail.com>
2022-10-14 17:46:26 +0530

    Optimized multiply_whole.asm.

--- src/library/multiply_whole.asm
+++ src/library/multiply_whole.asm
@@ -22,143 +22,97 @@ _multiply_whole:
   ;   - r14
   ;   - r15
   
-  push   rbx                 ; Push used callee-saved registers.
+  push   rbx                  ; Push used callee-saved registers.
   push   r12
   push   r13
   push   r14
   push   r15
-  mov    r9, rdi             ; Move char* a to r9.
-  mov    r10, rcx            ; Save rcx since strlen() doesn't preserve it.
-  call   strlen              ; rax = strlen(a)
-  mov    rbx, rax            ; Save rax in order to prevent it from being overwritten.
-  mov    rdi, rsi            ; strlen's first argument is now b.
-  call   strlen              ; Call strlen with b.
+  mov    r9, rdi               ; Move char* a to r9.
+  mov    r10, rcx              ; Save rcx since strlen() doesn't preserve it.
+  call   strlen                ; rax = strlen(a)
+  mov    rbx, rax              ; Save rax in order to prevent it from being overwritten.
+  mov    rdi, rsi              ; strlen's first argument is now b.
+  call   strlen                ; Call strlen with b.
   dec    rax
-  xchg   rax, rbx            ; rbx = strlen(b) - 1
-  mov    rdi, r9             ; Restore rdi.
-  mov    rcx, r10            ; Restore rcx.
-  xor    r10d, r10d          ; r10b is the carry.
-  mov    byte [rdx], '0'     ; Set the first character of the result string to '0'
-  xor    r14d, r14d          ; Set r14 to 0.
+  xchg   rax, rbx              ; rbx = strlen(b) - 1
+  mov    rdi, r9               ; Restore rdi.
+  mov    rcx, r10              ; Restore rcx.
+  xor    r10d, r10d            ; r10b is the carry.
+  mov    byte [rdx], '0'       ; Set the default value of the result string to '0'.
+  xor    r14d, r14d            ; Set r14 to 0.
 .loop_1:
-  xor    r13, r13
-  mov    r9, rax
+  lea    r13, [rax+1]          ; r13 = strlen(a) + 1
+  add    rcx, rax              ; rcx = &a[strlen(a)] (we're gonna have a carry 84% of the time)
+  mov    r9, rax               ; r9 = strlen(a)
+  mov    r15d, 10              ; The number we're dividing by.
+  push   rax                   ; Save rax since we use it for mul and div later.
 .loop_2:
-  mov    r15d, 10
-  mov    r11b, byte [rsi+rbx]
+  mov    r11b, byte [rsi+rbx]  ; r11b is a byte of char *b
   sub    r11b, '0'
-  mov    r12b, byte [rdi+r9-1]
+  mov    r12b, byte [rdi+r9-1] ; r12b is a byte of char *a
   sub    r12b, '0'
-  push   rax
-  push   rbx
-  push   rcx
-  push   rdx
-  mov    al, r11b
+  movzx  eax, r11b
   mul    r12b
-  mov    r11b, al
-  pop    rdx
-  pop    rcx
-  pop    rbx
-  pop    rax
-  add    r11b, r10b
-  push   rax
+  mov    r11b, al              ; r11b is the multiplication of char *a and char *b.
+  add    r11b, r10b            ; Add carry.
   push   rdx
-  push   rcx
-  push   rbx
-  mov    ax, 0
   mov    al, r11b
   div    r15b
-  pop    rbx
-  pop    rcx
-  mov    r10b, al
+  mov    r10b, al              ; carry = r11b / 10
   mov    al, ah
-  mov    byte [rcx+r13], al
-  add    byte [rcx+r13], '0'
+  mov    byte [rcx], al
+  add    byte [rcx], '0'       ; Add the remainder to the result string.
   pop    rdx
-  pop    rax
-  inc    r13
+  dec    rcx
   dec    r9
-  jnz    .loop_2
-  cmp    r10b, 0
-  jle    .after_if_1
-  mov    byte [rcx+r13], r10b
-  add    byte [rcx+r13], '0'
-  xor    r10b, r10b
-  inc    r13
-.after_if_1:
-  xor    r11, r11
-  push   rax ; optimize this too, MOVE INPUT!!
-  push   rdi
-  push   rcx
-  mov    rdi, rcx
-  call   strlen
-  mov    r13, rax
-  pop    rcx
-  pop    rdi
-  mov    r9, r13
-  shr    r9, 1
-.loop_3:
-  mov    al, byte [rcx+r11]
-  lea    r15, [rcx+r13-1]
-  sub    r15, r11
-  mov    r12b, byte [r15]
-  mov    byte [rcx+r11], r12b
-  mov    byte [r15], al
-  inc    r11
-  cmp    r11, r9
-  js     .loop_3
-  pop    rax
-  xor    r11, r11
+  jnz    .loop_2               ; Loop r9 times.
+  mov    byte [rcx], r10b      ; Add the final carry (might be 0)
+  add    byte [rcx], '0'       ; to the string.
+  xor    r10b, r10b            ; Reset carry.
+  xor    r11d, r11d            ; r11 = 0
   test   r14, r14
-  jle    .after_loop_4
-.loop_4:
-  mov    byte [rcx+r13], '0'
-  inc    r13
-  inc    r11
+  jle    .after_loop_3         ; if r14 <= 0 then skip loop_3
+.loop_3:                       ; Adds r14 trailing zeroes to buf1, r14 is the amount of times loop_1 has run.
+  mov    byte [rcx+r13], '0'   ; buf1[r13] = '0'
+  inc    r13                   ; r13++
+  inc    r11                   ; r11++
   cmp    r11, r14
-  js     .loop_4
-.after_loop_4:
-  mov    byte [rcx+r13], 0
+  js     .loop_3               ; Loop r14 times.
+.after_loop_3:
+  mov    byte [rcx+r13], 0     ; Zero-terminate the string to pass it to add_whole().
+  push   rcx                   ; Save used caller-saved registers.
   push   rdi
   push   rsi
   push   rdx
-  mov    rdi, rdx
-  mov    rsi, rcx
-  mov    rdx, r8
-  push   rax
-  push   rcx
-  push   r8
-  call   add_whole
-  pop    r8
-  pop    rcx
-  pop    rax
+  mov    rdi, rdx              ; param1 = rdx (char *res)
+  mov    rsi, rcx              ; param2 = rcx (char *buf1)
+  mov    rdx, r8               ; param3 = r8 (char *buf2)
+  mov    r15, r8               ; Saving the r8 register.
+  call   add_whole             ; buf2 = add_whole(res, buf1) (this doesn't actually return anything)
+  mov    r8, r15               ; Restoring the r8 register.
   pop    rdx
   pop    rsi
-  pop    rdi
-  xor    r11, r11
-  push   rax
-  push   rcx
-  push   rdi
+  xor    r11d, r11d
   mov    rdi, r8
-  call   strlen
+  call   strlen                ; r9 = strlen(buf2) (length of the addition result)
   mov    r9, rax
   pop    rdi
   pop    rcx
   pop    rax
-.loop_5:
+.loop_4:
   mov    r15b, byte [r8+r11]
   mov    byte [rdx+r11], r15b
   inc    r11
   cmp    r11, r9
-  js     .loop_5
+  js     .loop_4
   mov    byte [rdx+r9], 0
-  xor    r11, r11
-.loop_6:
+  xor    r11d, r11d
+.loop_5:
   mov    byte [rcx+r11], 0
   mov    byte [r8+r11], 0
   inc    r11
   cmp    r11, r13
-  js     .loop_6
+  js     .loop_5
   inc    r14
   mov    r15, rbx
   dec    rbx
