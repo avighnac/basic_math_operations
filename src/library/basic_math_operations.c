1c71af0925cc89bb44fd00896ca8527a7c00632e
Avighna <avighnakc@gmail.com>
2022-10-28 01:16:49 +0530

    Added a test which causes divide() to SIGSEGV.

--- src/library/basic_math_operations.c
+++ src/library/basic_math_operations.c
@@ -278,17 +278,18 @@ void divide_whole(const char *numerator, const char *denominator,
   extern size_t strlen(const char *str);
 
   size_t numerator_length = strlen(numerator);
+  size_t denominator_length = strlen(denominator);
   size_t modified_numerator_length = numerator_length + accuracy;
-  char *modified_numerator = (char *)calloc(numerator_length + accuracy + 1, 1);
+  char *modified_numerator = (char *)calloc(modified_numerator_length + 1, 1);
 
   for (size_t i = 0; i < numerator_length; i++)
     modified_numerator[i] = numerator[i];
   for (size_t i = numerator_length; i < accuracy + numerator_length; i++)
     modified_numerator[i] = '0';
 
-  size_t bufferSize = modified_numerator_length + strlen(denominator) + 2;
+  size_t bufferSize = modified_numerator_length + denominator_length + 2;
   char *buffer =
-      (char *)calloc(5 * bufferSize + 10 * (strlen(denominator) + 2) + 3, 1);
+      (char *)calloc(5 * bufferSize + 10 * (denominator_length + 2) + 3, 1);
   char *rem = _divide_whole_with_remainder(modified_numerator, denominator,
                                            quotient, bufferSize, buffer);
 
