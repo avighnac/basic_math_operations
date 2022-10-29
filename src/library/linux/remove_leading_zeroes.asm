5bda17bbd4b530721d4ae3d130144c4539e3dbd2
Avighna <avighnakc@gmail.com>
2022-10-29 17:14:26 +0530

    Added remove_leading_zeroes.asm for windows.

--- src/library/linux/remove_leading_zeroes.asm
+++ src/library/linux/remove_leading_zeroes.asm
@@ -16,7 +16,7 @@ remove_leading_zeroes:
   ;   - r11
 
   call   strlen
-  lea    r10, [rax - 1]
+  lea    r10, [rax-1]
   xor    r8b, r8b
   cmp    byte [rdi], 2DH
   sete   cl
@@ -26,7 +26,7 @@ remove_leading_zeroes:
   jz     .loop_1
   mov    byte [rsi], 2DH
 .loop_1:
-  cmp    byte [rdi + rdx], 48
+  cmp    byte [rdi+rdx], 48
   jz     .after_if_1
   mov    r8b, 1
 .after_if_1:
@@ -35,14 +35,14 @@ remove_leading_zeroes:
   or     r11b, r8b
   test   r11b, r11b
   jz     .after_if_2
-  mov    r11b, byte [rdi + rdx]
-  mov    byte [rsi + r9], r11b
+  mov    r11b, byte [rdi+rdx]
+  mov    byte [rsi+r9], r11b
   inc    r9
 .after_if_2:
   inc    rdx
   cmp    rdx, rax
   js     .loop_1
-  mov    byte [rsi + r9], 0
+  mov    byte [rsi+r9], 0
   ret
 global remove_leading_zeroes_inplace
 remove_leading_zeroes_inplace:
@@ -56,7 +56,7 @@ remove_leading_zeroes_inplace:
   ;   - edx
   ;   - r8
 
-  lea    rax, [rdi - 1]
+  lea    rax, [rdi-1]
 .loop_1:
   inc    rax
   cmp    byte [rax], 48
@@ -76,13 +76,13 @@ remove_leading_zeroes_inplace:
   push   rdi
   mov    rdi, rax
   call   strlen
-  lea    rcx, [rax + 1]
+  lea    rcx, [rax+1]
   pop    rdi
   pop    rax
   xor    r8d, r8d
 .loop_2:
-  mov    dl, byte [rax + r8]
-  mov    byte [rdi + r8], dl
+  mov    dl, byte [rax+r8]
+  mov    byte [rdi+r8], dl
   inc    r8
   cmp    r8, rcx
   js     .loop_2
