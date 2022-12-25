0286934140e980e1a2bc98677b4bb368f1b683bc
Avighna <avighnakc@gmail.com>
2022-12-25 23:38:43 +0530

    Implemented multiply_whole with dynamic memory allocation in assembly.
    Former-commit-id: d8b21334e1e21dc03099d766234357ece97b86a3

--- src/library/basic_math_operations.c
+++ src/library/basic_math_operations.c
@@ -174,16 +174,8 @@ void subtract(const char *a, const char *b, char *res) {
 
 extern void _multiply_whole(const char *a, const char *b, char *res, char *buf1,
                             char *buf2);
-void multiply_whole(const char* a, const char*b, char*res) {
-  extern size_t strlen(const char *str);
-  size_t a_length = strlen(a);
-  size_t b_length = strlen(b);
-  char* buf1 = (char*)calloc(a_length+b_length+1,1);
-  char* buf2 = (char*)calloc(a_length+b_length+1,1);
-  _multiply_whole(a, b, res, buf1, buf2);
-  free(buf1);
-  free(buf2);
-}
+extern void multiply_whole(const char *a, const char *b, char *res);
+
 void multiplyp(const char *a, const char *b, char *res) {
   extern size_t strlen(const char *str);
   size_t a_length = strlen(a);
