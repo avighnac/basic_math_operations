c49da68082b6671c85ebc432d9a902dd9ffde726
Aarav <aarav.malani@gmail.com>
2023-02-22 00:18:35 +0530

    fixed weird merge stuff

--- src/library/windows/strlen.asm
+++ src/library/windows/strlen.asm
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 section .text
 global  strlen
 strlen:
@@ -15,22 +14,4 @@ strlen:
   mov    r11b, byte[rcx + rax]
   test   r11b, r11b
   jnz    .loop
-=======
-section .text
-global  strlen
-strlen:
-  ; Input:
-  ;   - char *str -> rcx
-
-  ; Registers used:
-  ;   - r11
-
-  xor    r11d, r11d
-  mov    rax, -1
-.loop:
-  inc    rax
-  mov    r11b, byte[rcx + rax]
-  test   r11b, r11b
-  jnz    .loop
->>>>>>> two/main
   ret
