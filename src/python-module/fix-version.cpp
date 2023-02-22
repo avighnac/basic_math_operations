f252a89637e07d9c0db096b98e4e5d6e404d578a
avighnac <avighnakc@gmail.com>
2023-02-22 21:43:34 +0530

    Fixed versioning system.
    Former-commit-id: ff89ee271ef4c86c49f965c2a2d1d49578793913

---
+++ src/python-module/fix-version.cpp
@@ -0,0 +1,30 @@
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char **argv) {
+  if (argc < 2)
+    return 0;
+
+  std::ifstream in(argv[1]);
+  std::string version;
+  std::getline(in, version);
+
+  // 0.0.1-main.116.2397cac
+  // Find first occurence of '-'
+  version = version.substr(version.find('-') + 1, version.length());
+  // main.116.2397cac
+  // Find first occurence of '.'
+  version = version.substr(version.find('.') + 1, version.length());
+  // 116.2397cac
+  std::string patch = version.substr(0, version.find('.'));
+
+  std::string new_version = "0.0." + patch;
+
+  std::cout << new_version << std::endl;
+
+  // Rewrite version to same file.
+  in.close();
+  std::ofstream out(argv[1]);
+  out << new_version;
+  out.close();
+}
