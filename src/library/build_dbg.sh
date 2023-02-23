31ea399e4fc9df0e33f63e79cbb10339b1204c45
avighnac <avighnakc@gmail.com>
2023-02-23 16:49:33 +0530

    Fixed addp.asm.
    Former-commit-id: 47ccc151e8a8edc7bd61842c5771c931a55834e7

--- src/library/build_dbg.sh
+++ src/library/build_dbg.sh
@@ -4,8 +4,14 @@ do
   yasm -g dwarf2 -f elf64 $file
 done
 mv ./*.o ../
+cd ../cross-platform || exit
+for file in *.c
+do
+  gcc $file -g -c
+done
+mv ./*.o ../
 cd ..
-gcc basic_math_operations.c -O3 -c
+gcc basic_math_operations.c -g -c
 
 v=""
 for file in *.o
@@ -14,5 +20,7 @@ do
   v+=" "
 done
 
+gcc remove_zeroes.c -g -c
+
 g++ ../tests/test.cpp $v -I.
-rm ./*.o
+# rm ./*.o
