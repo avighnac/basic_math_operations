abe1cc167b1b1f0a3851ec3b8ffdd1ee15050609
avighnac <avighnakc@gmail.com>
2023-02-23 15:57:13 +0530

    Made .cpp file for basic_math_operations.
    Former-commit-id: 97821a052dbaff778213230d62cccfdde339e39c

--- src/library/basic_math_operations.hpp
+++ src/library/basic_math_operations.hpp
@@ -10,40 +10,20 @@ namespace basic_math_operations {
 /// @param a The first rational number to add as a decimal.
 /// @param b The second rational number to add as a decimal.
 /// @return The addition of the first two arguments.
-std::string add(std::string a, std::string b) {
-  char *additionResult = (char *)calloc(
-      a.length() + b.length() + 3,
-      1); // for the null byte, potential '-' sign, and addition carry
-  ::add(a.c_str(), b.c_str(), additionResult);
-  std::string answer = std::string(additionResult);
-  free(additionResult);
-  return answer;
-}
+std::string add(std::string a, std::string b);
+
 /// @brief Subtracts the second number from the first number and returns the
 /// result.
 /// @param a The first rational number as a decimal.
 /// @param b The rational number to subtract as a decimal.
 /// @return a - b, the result of the subtraction.
-std::string subtract(std::string a, std::string b) {
-  char *subtractionResult =
-      (char *)calloc(a.length() + b.length() + 3, 1); // same reason as above
-  ::subtract(a.c_str(), b.c_str(), subtractionResult);
-  std::string answer = std::string(subtractionResult);
-  free(subtractionResult);
-  return answer;
-}
+std::string subtract(std::string a, std::string b);
 
 /// @brief Multiplies the first two arguments and returns the result.
 /// @param a The first rational number as a decimal to multiply.
 /// @param b The second rational number as a decimal to multiply.
 /// @return The product of a and b.
-std::string multiply(std::string a, std::string b) {
-  char *product = (char *)calloc(a.length() + b.length() + 3, 1);
-  ::multiply(a.c_str(), b.c_str(), product);
-  std::string answer = std::string(product);
-  free(product);
-  return answer;
-}
+std::string multiply(std::string a, std::string b);
 
 /// @brief Divides the first number by the second number and returns the result.
 /// @param numerator The first rational number as a decimal to divide.
@@ -52,16 +32,7 @@ std::string multiply(std::string a, std::string b) {
 /// decimal places to calculate to.
 /// @return a / b, the result of a divided by b.
 std::string divide(std::string numerator, std::string denominator,
-                   size_t accuracy = 10) {
-  char *quotient =
-      (char *)calloc(numerator.length() + denominator.length() + accuracy + 3,
-                     1); // 2 for the potential negative signs in the inputs,
-                         // and 2 for the same reasons mentioned above.
-  ::divide(numerator.c_str(), denominator.c_str(), quotient, accuracy);
-  std::string answer = std::string(quotient);
-  free(quotient);
-  return answer;
-}
+                   size_t accuracy);
 
 /// @brief Returns the remainder when numerator is divided by denominator.
 /// @param numerator The rational numerator of the division represented as a
@@ -70,78 +41,30 @@ std::string divide(std::string numerator, std::string denominator,
 /// decimal.
 /// @return a % b, the remainder of the division of [numerator] and
 /// [denominator].
-std::string mod(std::string numerator, std::string denominator) {
-  char *quotient =
-      (char *)calloc(numerator.length() + denominator.length() + 3, 1);
-  // 2 for the potential negative signs in the inputs,
-  // and 2 for the same reasons mentioned above.
-  char *remainder =
-      (char *)calloc(std::max(numerator.length(), denominator.length()) + 1, 1);
-  divide_whole_with_remainder(numerator.c_str(), denominator.c_str(), quotient,
-                              remainder);
-  free(quotient);
-  std::string answer = std::string(remainder);
-  free(remainder);
-  return answer;
-}
+std::string mod(std::string numerator, std::string denominator);
 
 // constant that controls division accuracy
-size_t DIVISION_ACCURACY = 10;
+extern size_t DIVISION_ACCURACY;
 
 // BMONum
 class BMONum {
 public:
   std::string number;
 
-  BMONum() { number = "0"; }
-  BMONum(std::string number) { this->number = number; }
-  BMONum(const char *number) { this->number = number; }
+  BMONum();
+  BMONum(std::string number);
+  BMONum(const char *number);
 
-  BMONum operator+(BMONum n) {
-    BMONum answer;
-    answer.number = add(number, n.number);
-    return answer;
-  }
-  BMONum operator-(BMONum n) {
-    BMONum answer;
-    answer.number = subtract(number, n.number);
-    return answer;
-  }
-  BMONum operator*(BMONum n) {
-    BMONum answer;
-    answer.number = multiply(number, n.number);
-    return answer;
-  }
-  BMONum operator/(BMONum n) {
-    BMONum answer;
-    answer.number = divide(number, n.number, DIVISION_ACCURACY);
-    return answer;
-  }
-  BMONum operator%(BMONum n) {
-    BMONum answer;
-    answer.number = mod(number, n.number);
-    return answer;
-  }
+  BMONum operator+(BMONum n);
+  BMONum operator-(BMONum n);
+  BMONum operator*(BMONum n);
+  BMONum operator/(BMONum n);
+  BMONum operator%(BMONum n);
 
-  bool operator<(BMONum n) {
-    // return true if this < n, else false
-    // this < n ==> this - n < 0
-
-    std::string difference = subtract(number, n.number);
-    return difference[0] == '-';
-  }
-  bool operator>(BMONum n) {
-    // return true if this > n, else false
-    // this > n ==> n < this
-
-    return n < *this;
-  }
-  bool operator==(BMONum n) {
-    return number == n.number;
-  }
-  bool operator>=(BMONum n) {
-    return *this > n || *this == n;
-  }
+  bool operator<(BMONum n);
+  bool operator>(BMONum n);
+  bool operator==(BMONum n);
+  bool operator>=(BMONum n);
 
   friend std::ostream &operator<<(std::ostream &os, const BMONum &n) {
     os << n.number;
