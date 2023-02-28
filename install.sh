3c3949ccae52fb498b20e635bf5cf0953229ec8f
Aarav Malani <aarav.malani@gmail.com>
2023-02-28 09:17:52 +0530

    Change the install script

--- install.sh
+++ install.sh
@@ -1,9 +1,34 @@
+#!/bin/bash
+
 # Check if sudo, if not, exit
+
+COMMAND=wget
+
 if [ "$(id -u)" -ne 0 ]
-  then echo "Please run as root!"
+then 
+  echo -e "\e[32mPlease run as root!\e[0m"
   exit
 fi
 
+echo -e "Testing for unzip..."
+if ! command -v unzip &> /dev/null
+then
+  echo -e "\e[32munzip not installed!\e[0m"
+  exit
+fi
+
+if ! command -v unzip &> /dev/null
+then
+  echo -e "\e[32mwget not installed, defaulting to curl!\e[0m"
+  COMMAND="curl -O -J"
+  if ! command -v curl &> /dev/null
+  then
+    echo -e "\e[32mcurl not installed!\e[0m"
+    exit
+  fi
+fi
+
+latest
 cd /tmp
 # If the directory exists, delete it
 if [ -d "basic_math_operations-install" ]; then
@@ -11,11 +36,8 @@ if [ -d "basic_math_operations-install" ]; then
 fi
 mkdir basic_math_operations-install
 cd basic_math_operations-install
-curl -s https://api.github.com/repos/avighnac/basic_math_operations/releases/latest \
-| grep "browser_download_url.*zip" \
-| cut -d : -f 2,3 \
-| tr -d \" \
-| wget -qi -
+$COMMAND -q https://github.com/avighnac/basic_math_operations/releases/latest/download/libbasic_math_operations-linux.zip 
+
 
 echo "Sucessfully downloaded the latest release."
 
