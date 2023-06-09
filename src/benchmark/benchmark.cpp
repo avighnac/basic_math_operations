d4e33b34e40af529d1b7807061586b1518d7b32e
avighnac <avighnakc@gmail.com>
2023-06-09 17:25:09 +0530

    Finally removed that gigantic (painful) test submodule

--- src/benchmark/benchmark.cpp
+++ src/benchmark/benchmark.cpp
@@ -1,5 +1,6 @@
 #include "../library/basic_math_operations.hpp"
 #include <chrono>
+#include <random>
 #include <iostream>
 
 std::string factorial(int x) {
@@ -16,13 +17,39 @@ std::string factorial(int x) {
   return answer;
 }
 
+std::string add_random(size_t max_digits) {
+  // Generate two random strings of [0,9]'s with [max_digits - 20] +/- 20 digits
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(0, 20);
+
+  size_t digits = max_digits - dis(gen);
+  std::string a, b;
+  for (size_t i = 0; i < digits; i++) {
+    a += std::to_string(dis(gen));
+  }
+  digits = max_digits - dis(gen);
+  for (size_t i = 0; i < digits; i++) {
+    b += std::to_string(dis(gen));
+  }
+
+  char *ans = (char *)calloc(a.length() + b.length() + 1, sizeof(char));
+  multiply(a.c_str(), b.c_str(), ans);
+  std::string answer = ans;
+  free(ans);
+  return answer;
+}
+
 int main() {
+  #define MAX_TIME 5
+
   double time = 0;
   int x = 0;
 
-  while (time < 10) {
+  while (time < MAX_TIME) {
     auto start = std::chrono::high_resolution_clock::now();
-    std::string s = factorial(1000);
+    std::string s = add_random(200);
     auto end = std::chrono::high_resolution_clock::now();
     time += std::chrono::duration_cast<std::chrono::microseconds>(end - start)
                 .count() *
@@ -30,5 +57,5 @@ int main() {
     x++;
   }
 
-  std::cout << "1000!'s per second: " << x / 10 << '\n';
+  std::cout << "Random multiplications (200 digits max) per second: " << x / MAX_TIME << '\n';
 }
